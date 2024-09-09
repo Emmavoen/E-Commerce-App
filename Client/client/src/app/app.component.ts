b42f8a2dd8aca7199a0a31dcb9940780d67b4074
emmanuel <user@DESKTOP-TRNIJR7>
2024-09-09 17:47:48 +0100

    Added Pagination model

--- Client/client/src/app/app.component.ts
+++ Client/client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = 'App';
-  products: any[] = [];
+  title = 'App';  
+  products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,  private cdr: ChangeDetectorRef) {}
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products').subscribe({
-      next: (response: any) => this.products = response.data,
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/Products/GellAllProducts?ascending=true&pageSize=18').subscribe({
+      next: (response) => {
+        console.log(response); // Check the structure of the response
+        this.products = response.data;
+        this.cdr.detectChanges();},
       error: error => console.log(error),
       complete: () => {
         console.log('request completed');
